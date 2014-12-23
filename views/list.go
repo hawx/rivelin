d004345b625424054919279432c8ffdc6aa9e71a
Joshua Hawxwell <m@hawx.me>
2014-12-23 17:12:51 +0000

    Put template in separate package

---
+++ views/list.go
@@ -0,0 +1,160 @@
+package views
+
+import "html/template"
+
+var List = template.Must(template.New("list").Parse(list))
+
+const list = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Rivelin</title>
+    <style>
+      html, body {
+          margin: 0;
+          padding: 0;
+      }
+
+      body {
+          font: 14px/1.3 Verdana, Geneva, sans-serif;
+          color: #000;
+          background: #fff;
+          word-break: break-all;
+          hyphens: auto;
+      }
+
+      a, a:visited {
+          text-decoration: none;
+          color: #365da9;
+      }
+      a:hover, a:focus, a:active {
+          text-decoration: underline;
+          color: #2a6497;
+      }
+
+      .container {
+          max-width: 40em;
+          margin: 0 auto;
+          padding: 0 1em;
+      }
+      .container:before, .container:after {
+          clear: both;
+          content: " ";
+          display: table;
+      }
+
+      .page-title {
+          background: #eee;
+          border-bottom: 1px solid #ddd;
+          padding: 0;
+          margin: 0;
+      }
+      .page-title h1 {
+          font-size: 1.5em;
+          padding: 1.3rem;
+          margin: 0;
+          height: 1.3rem;
+          line-height: 1.3rem;
+          display: inline-block;
+          padding-left: 0;
+          font-weight: bold;
+      }
+
+      ul { list-style: none; padding: 0; }
+
+      .blocks {
+          width: auto;
+          margin: 2.6rem 0;
+      }
+
+      .block {
+          clear: both;
+          padding: .5rem 0 0;
+          border-top: 1px solid #ddd;
+          margin: 1.1rem 0 0;
+      }
+      .block-title h1, .block-title time {
+          float: left;
+          padding: 0 .5rem 0 0;
+          margin: -1.1rem 0 0;
+          font-size: .75rem;
+          font-weight: normal;
+          background: #fff;
+      }
+      .block-title .icon {
+          position: relative;
+          float: left;
+          margin: 0 .5rem 0 -1.5rem;
+          border: 0 none;
+          vertical-align: middle;
+      }
+      .block-title time {
+          float: right;
+          padding: 0 0 0 .5rem;
+          color: #777;
+      }
+
+      .item {
+          clear: both;
+          position: relative;
+          padding: 1rem 0;
+          margin: 0;
+      }
+      .item header {
+          margin: 0 0 .3rem;
+      }
+      .item h2 {
+          font-size: 1rem;
+          margin: 0;
+      }
+      .item p {
+          font-size: 0.875rem;
+          margin: .2rem 0;
+      }
+      .item time {
+          clear: both;
+          margin: 0 1.5rem 0 0;
+          font-size: .6875rem;
+          color: #666;
+      }
+
+      @media screen and (max-width: 40rem) {
+          .block .icon { display: none; }
+      }
+    </style>
+  </head>
+  <body>
+    <header class="page-title">
+      <div class="container">
+        <h1>rivelin</h1>
+      </div>
+    </header>
+
+    <div class="container">
+      <ul class="blocks">
+        {{range .UpdatedFeeds.UpdatedFeed}}
+        <li class="block">
+          <header class="block-title">
+            <h1>
+              <img class="icon" src="http://www.google.com/s2/favicons?domain={{.WebsiteUrl}}" alt="">
+              <a href="{{.WebsiteUrl}}">{{.FeedTitle}}</a>
+              (<a href="{{.FeedUrl}}">Feed</a>)
+            </h1>
+            {{.WhenLastUpdate.HtmlFormat}}
+          </header>
+          <ul class="items">
+            {{range .Item}}
+            <li class="item" id="{{.Id}}">
+              <h2><a rel="external" href="{{.Link}}">{{.Title}}</a></h2>
+              <p>{{.Body}}</p>
+              {{.PubDate.HtmlFormat}}
+            </li>
+            {{end}}
+          </ul>
+        </li>
+        {{end}}
+      </ul>
+    </div>
+  </body>
+</html>`
